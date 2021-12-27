af1296472527930ac036a12c615f256b19b7ce08
Thiago Nunes Monteiro <thiagomonteiro.trab@outlook.com>
2021-12-27 14:54:30 -0300

    Add internet verification to app

--- app/src/main/java/com/example/eventssicredi/ui/eventlistfragment/EventListFragment.kt
+++ app/src/main/java/com/example/eventssicredi/ui/eventlistfragment/EventListFragment.kt
@@ -1,9 +1,13 @@
 package com.example.eventssicredi.ui.eventlistfragment
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -11,7 +15,6 @@ import com.example.eventssicredi.databinding.EventListFragmentBinding
 import com.example.eventssicredi.model.EventEntity
 import com.example.eventssicredi.service.EventRepository
 import com.example.eventssicredi.utils.navigateWithAnimations
-import kotlinx.android.synthetic.main.event_detail_fragment.*
 import kotlinx.android.synthetic.main.event_list_fragment.*
 
 class EventListFragment : Fragment() {
@@ -35,7 +38,13 @@ class EventListFragment : Fragment() {
         viewModel = ViewModelProvider(
             this,
             EventListViewModel.EventListViewModelFactory(EventRepository()))[EventListViewModel::class.java]
-        viewModel.loadEvents()
+
+        // Check if we're running on Android 5.0 or higher
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+           loadItems()
+        } else {
+            viewModel.loadEvents()
+        }
 
         viewModel.events.observe(viewLifecycleOwner, {
             val eventListAdapter = EventListAdapter(it, viewModel).apply {
@@ -66,6 +75,29 @@ class EventListFragment : Fragment() {
             this.adapter = EventListAdapter(eventList, viewModel)
             it.adapter = this.adapter
         } ?: throw AssertionError()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    fun loadItems(){
+        if(isInternetConnected(requireContext())){
+            viewModel.loadEvents()
+        } else {
+            binding?.clWifiOff?.visibility = View.VISIBLE
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    fun isInternetConnected(getApplicationContext: Context): Boolean {
+        var status = false
+        val cm =
+            getApplicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (cm != null) {
+            if (cm.activeNetwork != null && cm.getNetworkCapabilities(cm.activeNetwork) != null) {
+                // connected to the internet
+                status = true
+            }
+        }
+        return status
     }
 
 }
