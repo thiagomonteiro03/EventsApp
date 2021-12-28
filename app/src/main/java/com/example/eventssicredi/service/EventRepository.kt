0d6158e2fa2249f0cb7c2dbab0adfe8caf4ac86c
Thiago Nunes Monteiro <thiagomonteiro.trab@outlook.com>
2021-12-28 11:49:08 -0300

    Fixing problems to other apis

--- app/src/main/java/com/example/eventssicredi/service/EventRepository.kt
+++ app/src/main/java/com/example/eventssicredi/service/EventRepository.kt
@@ -1,23 +1,26 @@
 package com.example.eventssicredi.service
 
+import android.util.Log
 import com.example.eventssicredi.model.Checkin
 import com.example.eventssicredi.model.EventEntity
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
-import okhttp3.ResponseBody
-import retrofit2.Call
 import retrofit2.Response
 
 class EventRepository : RepositoryInterface{
 
     override suspend fun getApiData(): Response<List<EventEntity?>> {
-         return withContext(Dispatchers.Default) {
-            val retrofitClient = RetrofitUtils
-                .getRetrofitInstance(RetrofitConstants.URL)
-            val endpoint = retrofitClient.create(RetrofitInterface::class.java)
-            endpoint.getEvents()
+        try {
+            return withContext(Dispatchers.Default) {
+                val retrofitClient = RetrofitUtils
+                    .getRetrofitInstance(RetrofitConstants.URL)
+                val endpoint = retrofitClient.create(RetrofitInterface::class.java)
+                endpoint.getEvents()
+            }
+        } catch (ex: ExceptionInInitializerError){
+            Log.e("getApiData error", ex.toString())
         }
+        return Response.success(listOf())
     }
 
     override suspend fun sendCheckin(userSend: Checkin): Response<Checkin> {
